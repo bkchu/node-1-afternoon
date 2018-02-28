ac379a9ded27b0cf9baf557003d0cbc77115691e
bkchu <brandon.k.chung@gmail.com>
2018-02-28 14:26:32 -0600

    trying to deploy to heroku

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const mc = require(__dirname + "/controllers/messages_controller");
+const serve = require("serve");
 const app = express();
 
 app.use(bodyParser.json());
